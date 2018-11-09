aeb76f33e44ff2464c9cb7fed52d6923f1ac138c
iengpho <ieng.pho@gaea-sys.com>
2018-11-09 11:21:37 +0700

    create navigation bar

--- KChic-Chat/ViewController/SignUpViewController.swift
+++ KChic-Chat/ViewController/SignUpViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    var userNamer: String = ""
+    var userPassword: String = ""
+    var userConfirmPassword: String = ""
+    var isAttemed: Bool = false
+    
+    var navBar: UINavigationBar = {
+        let nav = UINavigationBar()
+        let mframe = UIApplication.shared.statusBarFrame.size
+        let frame = CGRect(x: 0, y: mframe.height, width: mframe.width, height: 44)
+        let navItem = UINavigationItem(title: "Sign Up")
+        nav.frame = frame
+        nav.setItems([navItem], animated: false)
+        return nav
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        view.addSubview(navBar)
         
     }
 }
